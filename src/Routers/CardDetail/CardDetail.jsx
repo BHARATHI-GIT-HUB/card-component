6c97b3a948ceb73a15b3fdc239158bb824274f74
BHARATHI <88837161+BHARATHI-GIT-HUB@users.noreply.github.com>
2022-08-27 07:19:48 +0530

    button made sticky

--- src/Routers/CardDetail/CardDetail.jsx
+++ src/Routers/CardDetail/CardDetail.jsx
@@ -8,18 +8,18 @@ import Doller from "../../Assets/Icons/Doller";
 import { connect } from "react-redux";
 const CardDetail = ({ currentItem }) => {
   return (
-    <div className=" my-[6rem]  flex justify-center items-start gap-2  max-w-[1440px]">
-      <div className="hidden  tablet:flex  flex-col items-center justify-center gap-4  min-w-[416px] max-w-[514px] w-full px-[3rem]">
+    <div className=" flex  h-full max-w-[1440px] items-start justify-center gap-2  md:my-[6rem]">
+      <div className="hidden  w-full  min-w-[416px] max-w-[514px] flex-col items-center  justify-center gap-4 px-[3rem] tablet:flex">
         <img
           src={currentItem.image}
           alt="logo"
-          className=" h-[208px] w-[208px] object-cover rounded-full"
+          className=" h-[208px] w-[208px] rounded-full object-cover"
         />
-        <h1 className="gradient text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r  from-blue-400  via-violet-400  to-yellow-400 ">
+        <h1 className="gradient bg-gradient-to-r from-blue-400 via-violet-400 to-yellow-400 bg-clip-text  text-4xl  font-extrabold  text-transparent ">
           {currentItem.heading}
         </h1>
-        <div className=" flex flex-col justify-center items-center gap-4 ">
-          <span className=" text-base font-medium  text-textColor leading-[1.5rem]">
+        <div className=" flex flex-col items-center justify-center gap-4 ">
+          <span className=" text-base font-medium  leading-[1.5rem] text-textColor">
             Project budget:
           </span>
           <span className=" text-2xl font-semibold leading-3">
@@ -27,57 +27,57 @@ const CardDetail = ({ currentItem }) => {
           </span>
           <p className="text-base  font-medium leading-[1.3rem] text-textColor">
             This is a{" "}
-            <span className="text-textHighlight font-bold ">fixed price</span>{" "}
+            <span className="font-bold text-textHighlight ">fixed price</span>{" "}
             Opportunity
           </p>
-          <button className=" font-semibold text-white  bg-black font-base px-[48px] py-[10px]  rounded-full flex gap-2 ">
+          <button className=" font-base flex  gap-2 rounded-full bg-black px-[48px]  py-[10px] font-semibold text-white ">
             <Star /> Apply To Opportunity
           </button>
-          <span className="text-base  font-medium leading-3 text-textColor flex gap-1">
+          <span className="flex  gap-1 text-base font-medium leading-3 text-textColor">
             <h1 className="font-normal tracking-normal">Timeline:</h1>
-            <span className="text-textHighlight font-bold tracking-wide ">
+            <span className="font-bold tracking-wide text-textHighlight ">
               {currentItem.weeks}
             </span>{" "}
           </span>
           <span
-            className="text-base  font-medium  text-textColor 
-            flex gap-1"
+            className="flex  gap-1  text-base 
+            font-medium text-textColor"
           >
             {" "}
-            <h1 className="font-normal tracking-normal leading-3">
+            <h1 className="font-normal leading-3 tracking-normal">
               Start date:
             </h1>{" "}
-            <span className="text-textHighlight font-bold tracking-normal leading-3 ">
+            <span className="font-bold leading-3 tracking-normal text-textHighlight ">
               Immediately
             </span>
           </span>
         </div>
       </div>
       {/* <div className="flex flex-col gap-[5rem] justify-center items-center "> */}
-      <div className="felx flex-col flex-auto justify-center items-center  ">
-        <div className=" bg-transparent tablet:bg-[#F9F9F9] px-[16px] py-[24px] tablet:px-[54px]  tablet:py-[32px] flex tablet:flex-auto flex-col justify-center items-center gap-6 relative rounded-2xl ">
-          <div className="flex flex-col justify-center  items-start gap-6 py-[54px] ">
+      <div className="felx flex-auto flex-col items-center justify-center  ">
+        <div className=" relative flex flex-col items-center justify-center  gap-6 rounded-2xl bg-transparent px-[16px] py-[24px] tablet:flex-auto tablet:bg-[#F9F9F9] tablet:px-[54px] tablet:py-[32px] ">
+          <div className="flex flex-col items-start  justify-center gap-6 md:py-[54px] ">
             <Link
               to={"/"}
-              className="text-[1rem]  font-bold leading-3 text-textColor_1 flex gap-1 justify-center items-center"
+              className="flex  items-center justify-center gap-1 text-[1rem] font-bold leading-3 text-textColor_1"
             >
               <BackArrow />
               Back to Opportunities{" "}
             </Link>
-            <p className=" py-[.3rem] px-[1rem] bg-[#EEEEEE] rounded-full text-sm text-bold text-textColor  flex  items-center gap-2">
+            <p className=" text-bold flex items-center gap-2 rounded-full bg-[#EEEEEE] py-[.3rem]  px-[1rem]  text-sm text-textColor">
               <Calander />
               Posted {currentItem.days} days ago
             </p>
-            <h1 className=" text-3xl sm:text-4xl font-semibold tracking-[0.25px] leading-[40px] ">
+            <h1 className=" text-3xl font-semibold leading-[40px] tracking-[0.25px] sm:text-4xl ">
               3D Design at Icarus
             </h1>
             <span className="flex gap-2">
-              <p className=" py-[6px] px-[10px] text-xs sm:text-sm bg-SecondaryWhite rounded-full  text-[#2e2e2eef] text-bold flex   items-center gap-2 whitespace-nowrap">
+              <p className=" text-bold flex items-center gap-2 whitespace-nowrap rounded-full  bg-SecondaryWhite py-[6px] px-[10px]   text-xs text-[#2e2e2eef] sm:text-sm">
                 {currentItem.roles_1}{" "}
               </p>
               <div>
                 {currentItem.roles_2.length > 0 ? (
-                  <p className=" py-[6px] px-[10px] text-xs sm:text-sm bg-[#EEEEEE] rounded-full text-textDark flex   items-center gap-2  overflow-hidden whitespace-nowrap text-ellipsis ">
+                  <p className=" flex items-center gap-2 overflow-hidden text-ellipsis whitespace-nowrap rounded-full bg-[#EEEEEE]   py-[6px] px-[10px]  text-xs text-textDark sm:text-sm ">
                     {currentItem.roles_2}
                   </p>
                 ) : (
@@ -85,114 +85,114 @@ const CardDetail = ({ currentItem }) => {
                 )}
               </div>
             </span>
-            <div className="flex flex-col  sm:flex-row gap-8 justify-center items-center">
-              <div className="flex gap-3 justify-center items-center">
+            <div className="flex flex-col  items-center justify-center gap-8 sm:flex-row">
+              <div className="flex items-center justify-center gap-3">
                 <img
                   src={currentItem.image}
                   alt="Logo"
-                  className=" w-[48px] h-[48px] md:w-[64px] md:h-[64px] object-cover rounded-full"
+                  className=" h-[48px] w-[48px] rounded-full object-cover md:h-[64px] md:w-[64px]"
                 />
                 <div>
-                  <p className="gradiant font-semibold text-xl   tracking-[-0.15px] ">
+                  <p className="gradiant text-xl font-semibold   tracking-[-0.15px] ">
                     {currentItem.heading}
                   </p>
-                  <p className="  text-sm text-textColor flex    gap-2 leading-[1.4rem]">
+                  <p className="  flex gap-2 text-sm    leading-[1.4rem] text-textColor">
                     Company
                   </p>
                 </div>
               </div>
-              <div className="flex gap-3 justify-center items-center">
+              <div className="flex items-center justify-center gap-3">
                 <img
                   src={currentItem.image}
                   alt="Logo"
-                  className=" w-[48px] h-[48px] md:w-[64px] md:h-[64px] object-cover rounded-full"
+                  className=" h-[48px] w-[48px] rounded-full object-cover md:h-[64px] md:w-[64px]"
                 />
                 <div>
-                  <p className=" font-semibold text-xl   tracking-[-0.15px]">
+                  <p className=" text-xl font-semibold   tracking-[-0.15px]">
                     {currentItem.heading}
                   </p>
-                  <p className="  text-sm text-textColor leading-[1.4rem]">
+                  <p className="  text-sm leading-[1.4rem] text-textColor">
                     Company
                   </p>
                 </div>
               </div>
             </div>
             <div className="flex flex-col  items-center gap-2 tablet:hidden">
-              <span className=" flex  self-start items-center gap-1">
+              <span className=" flex  items-center gap-1 self-start">
                 <Doller />
 
                 <span className=" text-lg font-light">{currentItem.money}</span>
 
-                <p className=" text-lg font-light  list-disc">Fixed</p>
+                <p className=" list-disc text-lg  font-light">Fixed</p>
               </span>
-              <span className=" flex justify-center items-center gap-1">
+              <span className=" flex items-center justify-center gap-1">
                 <Clock />
 
                 <span className=" text-lg font-light">Less than one Week</span>
 
-                <p className=" text-lg font-light  list-disc">Imm</p>
+                <p className=" list-disc text-lg  font-light">Imm</p>
               </span>
             </div>
-            <div className="border-t border-[#E3E3E3] mt-3 w-full"></div>
+            <div className="mt-3 w-full border-t border-[#E3E3E3]"></div>
             <div className=" py-3">
-              <h1 className=" font-semibold text-md text-textDark leading-[2rem] ">
+              <h1 className=" text-md font-semibold leading-[2rem] text-textDark ">
                 Project Deliverables
               </h1>
 
               <ul>
-                <li className="font-semibold text-md text-textHighlight list-disc leading-[1.5rem] ">
+                <li className="text-md list-disc font-semibold leading-[1.5rem] text-textHighlight ">
                   {currentItem.project_deivery_1}{" "}
                 </li>
               </ul>
-              <p className="font-normal text-md text-textColor_1 leading-[2.6rem]">
+              <p className="text-md font-normal leading-[2.6rem] text-textColor_1">
                 {currentItem.project_deivery_2}{" "}
               </p>
             </div>
-            <div className="border-t border-[#E3E3E3]  w-full"></div>
+            <div className="w-full border-t  border-[#E3E3E3]"></div>
             <div className="py-3">
-              <h1 className=" font-semibold text-md text-textDark leading-[1.4rem] ">
+              <h1 className=" text-md font-semibold leading-[1.4rem] text-textDark ">
                 Description
               </h1>
               <span className=" flex flex-col gap-1">
-                <p className="font-normal text-md text-textColor_1 leading-[2.6rem]">
+                <p className="text-md font-normal leading-[2.6rem] text-textColor_1">
                   {currentItem.discription}
                 </p>
               </span>
             </div>
-            <div className="border-t border-[#E3E3E3]  w-full"></div>
+            <div className="w-full border-t  border-[#E3E3E3]"></div>
             <div className=" py-3">
-              <h1 className=" font-semibold text-md text-textDark leading-[1.4rem] ">
+              <h1 className=" text-md font-semibold leading-[1.4rem] text-textDark ">
                 Company Description
               </h1>
 
-              <p className="font-normal text-md text-textColor_1 leading-[2.6rem]">
+              <p className="text-md font-normal leading-[2.6rem] text-textColor_1">
                 {currentItem.company_discription}
               </p>
             </div>
-            <div className="border-t border-[#E3E3E3]  w-full"></div>
+            <div className="w-full border-t  border-[#E3E3E3]"></div>
           </div>
           <div className=" relative bottom-[-3.25rem]  ">
             <img
               src={currentItem.image}
               alt=""
-              className="w-[48px] h-[48px] object-cover rounded-full hidden sm:block"
+              className="hidden h-[48px] w-[48px] rounded-full object-cover sm:block"
             />
           </div>
         </div>
-        <div className="flex flex-col justify-center items-center gap-2 mt-[3rem]">
-          <div className=" text-center hidden sm:block">
-            <h1 className=" font-bold text-xl text-textDark  ">
+        <div className="mt-[3rem] flex flex-col items-center justify-center gap-2">
+          <div className=" hidden text-center sm:block">
+            <h1 className=" text-xl font-bold text-textDark  ">
               Landon Sanford
             </h1>
-            <p className=" text-md text-textColor_1  font-normal">
+            <p className=" text-md font-normal  text-textColor_1">
               we create web3 platforms
             </p>
           </div>
-          <div className="bg-SecondaryWhite fixed̃ bottom-3 w-full px-4 py-3 sm:bg-transparent flex justify-center items-center  ">
-            <button className=" font-semibold text-white  bg-black font-[1rem] px-[28px] py-[10px]  my-2 rounded-full  gap-2 w-full sm:w-max flex justify-center items-center  ">
-              <Star /> Apply
-            </button>
-          </div>
+        </div>
+        <div className="sticky  bottom-0  flex w-full items-center justify-center bg-SecondaryWhite px-4 pt-2 pb-4 sm:bg-transparent md:static    ">
+          <button className=" font-[1rem] my-2  flex w-full items-center justify-center  gap-2 rounded-full  bg-black px-[28px] py-[10px] font-semibold text-white sm:w-max  ">
+            <Star /> Apply
+          </button>
         </div>
       </div>
     </div>
